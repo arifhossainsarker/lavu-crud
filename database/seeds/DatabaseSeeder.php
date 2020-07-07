49a9dca2b03a1d63574b980f10caabda5e06b9c3
Arif Hossain <arifhossain393@gmail.com>
2020-07-07 11:50:17 +0600

    customer model create

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(App\Customer::class, 10)->create();
     }
 }
